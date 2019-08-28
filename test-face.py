f5f7a1e1e5f5ac7c63b6b409f706282f33d34b35
Konstantin <woodprop@yandex.ru>
2019-08-28 15:14:02 +0300

    real-time face detection

--- test-face.py
+++ test-face.py
@@ -5,19 +5,25 @@ face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fronta
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
 
-cap = cv2.VideoCapture(url)
-ret, img = cap.read()
+cap = cv2.VideoCapture(0)
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-for (x, y, w, h) in faces:
-    img = cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
-    eyes = eye_cascade.detectMultiScale(roi_gray)
-    for (ex, ey, ew, eh) in eyes:
-        cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+while cap.isOpened():
+    ret, img = cap.read()
+    # frame = cv2.resize(frame, (800, 600))  # приводим разрешение к выходному, иначе на выходе пустой файл
 
-# cv2.imshow('img', img)
-cv2.imwrite('face.png', img)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        img = cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+    if ret:
+        cv2.imshow('img', img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break  # для завершения записи нажать 'q' на клаве
+
+# cv2.imwrite('face.png', img)
 
